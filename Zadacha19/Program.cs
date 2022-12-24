5cff1665cc53931a8dbd9618342b2ddc681327f8
Nina <you@example.com>
2022-12-24 20:28:02 +0300

    Решение задач окончательно

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -1,28 +1,27 @@
-﻿Console.WriteLine("Введите число:");
-int number = int.Parse(ConsoleReadLine());
-int number= new int[5];
-if (number.Length == 5)
+﻿System.Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+if(str.Length != 5)
 {
-    number[0] =number[4] && number[1] = number[3];
-   
-    Console.WriteLine($"{number}->да");
+   System.Console.WriteLine("Это не пятизначное  число");
+   Environment.Exit(0);
+}
+int[] A = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    A[i] = int.Parse(str[i].ToString());
+    //System.Console.WriteLine(A[i]);
 }
 
+if (A[0] == A[4] && A[1] == A[3])
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    System.Console.WriteLine("Нет");
+}
 
 
 
--- Zadacha19/Zadacha23/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿System.Console.Write("Введите число: ");
-int x=Convert.ToInt32(Console.ReadLine());
-for (int i=1; i<=x;i++)
-{
-    System.Console.WriteLine(Math.Pow (i,3));
-}
